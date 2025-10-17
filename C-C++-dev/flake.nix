c64400d7b7510807028ec4c921b182263143851f
vilma969 <ammiguel012@gmail.com>
2025-10-17 13:22:00 +0100

    idk

--- C-C++-dev/flake.nix
+++ C-C++-dev/flake.nix
@@ -6,16 +6,16 @@
     outputs = { self, nixpkgs }:
     let
         system = "x86_64-linux";
-        pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; } };
+        pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
     in
     {
         devShells.${system}.default = pkgs.mkShell {
             buildInputs = [
-                pkgs.vscode-fhs
                 pkgs.gcc
                 pkgs.gdb
             ];
             shellHook = ''
+                export PATH=$PATH:/home/afma/.vscode-server/bin/03c265b1adee71ac88f833e065f7bb956b60550a/bin/remote-cli/code
                 echo "C/C++ development environment ready"
             '';
         };
