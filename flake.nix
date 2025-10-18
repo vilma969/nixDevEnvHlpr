b14a49d7bf3ea81f13e673330a169401ae00aebf
vilma969 <ammiguel012@gmail.com>
2025-10-18 17:30:35 +0100

    idk anymore

--- flake.nix
+++ flake.nix
@@ -1,56 +1,48 @@
 {
-    description = "nixDevEnvHelper";
+  description = "nixDevEnvHelper";
 
-    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    outputs = { self, nixpkgs }:
-    let
-        system = "x86_64-linux";
-        pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
-    in
-    {
-        devShells.${system}.default = pkgs.mkShell {
-            buildInputs = [
-                pkgs.vscode-fhs
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        pkgs.python313
+        pkgs.python313Packages.pip
 
-                pkgs.python313
-                pkgs.python313Packages.pip
+        pkgs.jdk25
+        pkgs.maven
 
-                pkgs.jdk25
-                pkgs.maven
+        pkgs.gcc
+        pkgs.gdb
+      ];
+      shellHook = ''
+        mkdir -p /tmp/pip-packages
+        # mkdir -p /tmp/maven-packages
 
-                pkgs.gcc
-                pkgs.gdb
-            ];
-            shellHook = ''
-                mkdir -p /tmp/pip-packages
-                mkdir -p /tmp/maven-packages
+        # ------------------------
 
-                # --------
+        export PYTHONUSERBASE=/tmp/pip-packages
+        export PIP_TARGET=/tmp/pip-packages
 
-                export PYTHONUSERBASE=/tmp/pip-packages
-                export PIP_TARGET=/tmp/pip-packages
+        # export MAVEN_OPTS="-Dmaven.repo.local=/tmp/maven-packages"
 
-                export MAVEN_OPTS="-Dmaven.repo.local=/tmp/maven-packages"
-
-                # --------
+        # ------------------------
                 
-                echo "Python development environment ready"
-                echo "PYTHONUSERBASE and PIP_TARGET set to /tmp/pip-packages"
+        echo "PYTHONUSERBASE and PIP_TARGET set to /tmp/pip-packages"
+        echo "MAVEN_OPTS set to /tmp/maven-packages"
 
-                echo "Java development environment ready"
-                echo "MAVEN_OPTS set to /tmp/maven-packages"
+        # ------------------------
 
-                echo "C/C++ development environment ready"
+        python3 -m pip install -r requirements.txt
+        python3 app.py
 
-                # --------
-
-                VSCODE_ROOT_DATA="/tmp/vscode-root-data-${USER:-root}"
-                mkdir -p "${VSCODE_ROOT_DATA}"
-                chmod 0700 "${VSCODE_ROOT_DATA}"
-                
-                code --user-data-dir="${VSCODE_ROOT_DATA}" --no-sandbox
-            '';
-        };
+        java App.java
+      '';
     };
+  };
 }
--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -p "Enter username to run VS Code as: " USERNAME
-
-if ! id "$USERNAME" &>/dev/null; then
-    sudo useradd -m "$USERNAME"
-fi
-
-sudo -u "$USERNAME" code
